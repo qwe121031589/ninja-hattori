fc18dc4948eb9d7652c39e02b08f9aca0e2e02ca
qwe121031589 <64921305+qwe121031589@users.noreply.github.com>
2021-03-19 23:05:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const linebotParser = require('./bot.event')
 // Connect to MongoDB
 mongoose
   .connect(
-    '請輸入資料庫網址(mongo)',
+    'mongodb+srv://watame:<password>@cluster0.3ozhv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     { useNewUrlParser: true }
   )
   .then(() => console.log('MongoDB Connected'))
