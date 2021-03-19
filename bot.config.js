401b4d020fce135958228c6f1aa78769b26cb17c
qwe121031589 <64921305+qwe121031589@users.noreply.github.com>
2021-03-19 23:07:39 +0800

    Update bot.config.js

--- bot.config.js
+++ bot.config.js
@@ -1,9 +1,9 @@
 const linebot = require('linebot');
 
 var bot = linebot({
-    channelId: '請輸入',
-    channelSecret: '請輸入',
-    channelAccessToken: '請輸入',
+    channelId: '1655776083',
+    channelSecret: '213a62dbacecaa16ba85c566b84add27',
+    channelAccessToken: 'COJLOiPSV+RBKuyu39TFlkGO30m0wCImwrz8mbgmpkP5myqNjcRTcGkAe2QiSFgpOYHdyU8l87QkJDbiDeRyc17nWyXHW8jRlsVgVFe4yp5euB9NsmOXJlsgIhvOHM0peTNgEKpRX7PPiOiOgvod9AdB04t89/1O/w1cDnyilFU=',
 })
 
 module.exports = bot
